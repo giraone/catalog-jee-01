072aa2932f281f8d0fba7a1fb09993b15c201e1e
giraone <hans.schuell@giraone.com>
2015-12-15 10:13:57 +0100

    Specific overwrite because we have 2 Application classes

--- CatalogService/src/main/java/com/giraone/samples/catalog1/boundary/AdminApi.java
+++ CatalogService/src/main/java/com/giraone/samples/catalog1/boundary/AdminApi.java
@@ -1,5 +1,8 @@
 package com.giraone.samples.catalog1.boundary;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
@@ -7,4 +10,15 @@ import javax.ws.rs.core.Application;
 public class AdminApi extends Application
 {
 	public final static String PERSISTENCE_UNIT = "primary";
+
+	/* Override because we have two base pathes.
+	 * See http://www.adam-bien.com/roller/abien/entry/multiple_jax_rs_uris_in
+	 */
+	@Override
+	public Set<Class<?>> getClasses()
+	{
+		Set<Class<?>> resources = new HashSet<>();
+		resources.add(ProviderEndpoint.class);
+		return resources;
+	}
 }
