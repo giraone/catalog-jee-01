6ea3ac041a27ca092cb3cc9ecb3e5cb04e7d5a05
Hans Schuell <Hans Schuell>
2016-01-05 22:48:22 +0100

    CORS comment fixed

--- CatalogService/src/main/java/com/giraone/samples/common/boundary/BaseEndpoint.java
+++ CatalogService/src/main/java/com/giraone/samples/common/boundary/BaseEndpoint.java
@@ -52,8 +52,8 @@ public class BaseEndpoint
 	@OPTIONS
 	public Response options()
 	{
-		return Response.status(Response.Status.NO_CONTENT).header(CORS_ALLOW_ORIGIN_HEADER, "*") // "127.0.0.1" does not
-																									// work
+		return Response
+			.status(Response.Status.NO_CONTENT).header(CORS_ALLOW_ORIGIN_HEADER, "*") // "127.0.0.1" does not work
 			.header(CORS_ALLOW_METHODS_HEADER, "GET, POST, DELETE, PUT, OPTIONS") // * THIS DOES NOT WORK HERE!
 			.header(CORS_ALLOW_REQUEST_HEADER, "content-type").build();
 	}
@@ -63,8 +63,8 @@ public class BaseEndpoint
 	@Path("{path:.*}")
 	public Response optionsAll(@PathParam("path") String path)
 	{
-		return Response.status(Response.Status.NO_CONTENT).header(CORS_ALLOW_ORIGIN_HEADER, "*") // "127.0.0.1" does not
-																									// work
+		return Response
+			.status(Response.Status.NO_CONTENT).header(CORS_ALLOW_ORIGIN_HEADER, "*") // "127.0.0.1" does not work
 			.header(CORS_ALLOW_METHODS_HEADER, "GET, POST, DELETE, PUT, OPTIONS") // * THIS DOES NOT WORK HERE!
 			.header(CORS_ALLOW_REQUEST_HEADER, "content-type").build();
 	}
